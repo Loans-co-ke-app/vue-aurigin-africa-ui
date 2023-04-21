e0e017b935641b9c424707f7d4fe84c6e8da2ccf
Felix Orinda <forinda82@gmail.com>
2023-04-21 15:15:40 +0300

    Update homepage

--- src/layouts/ChatLayout.vue
+++ src/layouts/ChatLayout.vue
@@ -5,7 +5,7 @@ import Sidebar from '@/components/chats/ChatSidebar.vue'
   <div>
     <div class="chat-layout">
       <div class="chat-layout__sidebar">
-        <sidebar />
+        <sidebar ></sidebar>
       </div>
       <div class="chat-layout__main">
         <router-view />
